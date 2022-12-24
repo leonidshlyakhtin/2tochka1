5145df0d48131897bc2d64ad4a9cc42bb01b0398
lenya <hellchildgg@mail.ru>
2022-12-24 09:37:04 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 750;
+            Console.WriteLine("Введите стоимость вашей покупки:");
+            double.TryParse(Console.ReadLine(), out price);
+            if (price > 400 && price < 600) price = price - price * .05;
+            else if (price >= 600 && price < 1000) price = price - price * .1;
+            Console.WriteLine($"Цена вашей покупки со скидкой: {price}");
+
+            Console.Read();
+        }
+    }
+}
